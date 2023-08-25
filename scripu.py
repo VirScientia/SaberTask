25d9129489bfb24db503a08f6e780f75ac219128
VirScientia <139371989+VirScientia@users.noreply.github.com>
2023-08-25 21:55:25 +0300

    Update scripu.py

--- scripu.py
+++ scripu.py
@@ -1,2 +1,39 @@
-if __nam__ == "__main__":
-    print("heh")
+import argparse
+import shutil
+
+import json
+from pathlib import Path
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Tool to merging two logs.')
+
+    parser.add_argument(
+        'input_log1',
+        metavar='<INPUT LOG FILE>',
+        type=str,
+        help='path to first file with generated logs for merging',
+    )
+
+    parser.add_argument(
+        'input_log1',
+        metavar='<OUTPUT DIR>',
+        type=str,
+        help='path to first file with generated logs for merging',
+    )
+
+    parser.add_argument(
+        '-o', '--output',
+        action='store',
+        default='1',
+        dest='output_dir',
+        type=str,
+        help='',
+    )
+
+    #return parser.parse_args()
+    return parser
+
+
+if __name__ == "__main__":
+    print(_parse_args()._actions)
